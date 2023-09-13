64e3f36a115b735b2e1003739cee68461e9f17a4
José Leite <joseleite688@gmail.com>
2023-09-13 15:58:20 +0100

    Melhorando javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,43 @@
 document.getElementById('cep-button').addEventListener('click', function () {
-    const apiKey = 'dff97f836e1b47d69613432b7adf154f';
-    const cep = document.getElementById('cep-input').value;
+    const cp = document.getElementById('cep-input').value;
 
-    const apiUrl = `https://www.cttcodigopostal.pt/api/v1/${apiKey}/${cep}`;
+    const apiUrl = `https://json.geoapi.pt/codigo_postal/${cp}?json=1`;
 
     fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao fazer a solicitação. Status: ' + response.status);
-            }
-            return response.json();
-        })
+    .then(response => {
+        if (!response.ok) {
+           throw new Error('Erro ao fazer a solicitação. Status: ' + response.status);
+        }
+        return response.json();
+    })
+        
         .then(data => {
-            console.log(data); // Mostra os dados da API no console
-            const resultsDiv = document.getElementById('cep-results');
-            resultsDiv.innerHTML = '';
+        const resultsDiv = document.getElementById('cep-results');
+        resultsDiv.innerHTML = '';
+          
+        if (data.ruas || data.Concelho ||data.Distrito||data.centro !== undefined) {
+            const address = data.ruas
+            const Municipality = data.Concelho
+            const District = data.Distrito
+            const coordinated = data.centro[0] + ', ' + data.centro[1]
 
-            if (Array.isArray(data) && data.length > 0) {
-                data.forEach(result => {
-                    const address = result.morada;
-                    const locality = result.localidade;
-                    const postalCode = result['codigo-postal'];
-                    const infoLocal = result['info-local'];
-                    const resultHTML = `
-                        <p><strong>Endereço:</strong> ${address}</p>
-                        <p><strong>Localidade:</strong> ${locality}</p>
-                        <p><strong>Código Postal:</strong> ${postalCode}</p>
-                        <p><strong>Informação Adicional:</strong> ${infoLocal}</p>
-                    `;
+            const resultHTML = `
+                <p><strong>Endereço:</strong> ${address}</p>
+                <p><strong>Concelho:</strong> ${Municipality}</p>
+                <p><strong>Distrito:</strong> ${District}</p>
+                <p><strong>Coordenadas:</strong> ${coordinated}</p>
+            `;
 
-                    resultsDiv.innerHTML += resultHTML;
-                });
-            } else {
-                resultsDiv.innerHTML = '<p>Nenhum resultado encontrado.</p>';
-            }
-        })
-        .catch(error => {
-            console.error('Ocorreu um erro ao processar a consulta:', error);
-            // Exiba uma mensagem de erro amigável para o usuário se desejar
-        });
+            resultsDiv.innerHTML = resultHTML;
+        } else {
+            resultsDiv.innerHTML = '<p>Nenhum resultado encontrado.</p>';
+        }
+    })
+    .catch(error => {
+        console.error('Ocorreu um erro ao processar a consulta:', error);
+        window.alert('Verifique o código postal informado.', error);
+        //limpar o campo
+        document.getElementById('cep-input').value = '';
+        document.getElementById('cep-results').innerHTML = '';
+    });
 });
